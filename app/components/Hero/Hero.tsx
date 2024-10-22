ad6369dcacd22f56ba63d131f58264305c3088f9
Hanees <ahmed.54.hanees31@gmail.com>
2024-10-22 16:59:10 +0100

    Adding image into hero section

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
+
 const Hero = () => {
   return (
-    <section className='flex flex-col items-center justify-center min-h-screen'>
-      <div className="text-center p-8 bg-white bg-opacity-25 rounded-xl">
-        <h1 className="text-5xl font-bold mb-4">Welcome to WAIS Autos</h1>
-        <p className="text-xl">Helping you upgrade your interior</p>
-        <button className="mt-6 px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-purple-500 transition duration-300">
-          Get Started
-        </button>
-      </div>
-    </section>
+<section className='flex flex-col md:flex-row items-center justify-center min-h-screen'>
+  
+  <div className="md:w-1/2 text-center md:text-left p-8 ml-5">
+    <h1 className="text-5xl font-bold mb-4">Welcome to WAIS Autos</h1>
+    <p className="text-xl">Helping you upgrade your interior</p>
+    <button className="mt-6 px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-purple-500 transition duration-300">
+      Get Started
+    </button>
+  </div>
+
+  
+  <div className="md:w-1/2 flex justify-center mt-6 md:mt-0">
+    Image
+  </div>
+</section>
+
   
   );
 };
