f3d4254e30f18596c0e2f6a7941dc61a11e1a5be
Hanees <ahmed.54.hanees31@gmail.com>
2024-11-03 20:42:28 +0000

    api sends email

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,3 +1,4 @@
+import { error } from 'console';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
@@ -5,8 +6,33 @@ export async function POST(request: Request) {
     const formData = await request.json(); 
     console.log("Form Data Received:", formData);
 
+    const nodemailer = require('nodemailer')
 
+    const transporter = nodemailer.createTransport({
+      service:'gmail',
+      auth:{
+        user: process.env.MY_EMAIL,
+        pass: process.env.EMAIL_PASS
+      }
+    })
+
+    const mailOptions = {
+      from: process.env.MY_EMAIL,
+      to: process.env.EMAIL_TO,
+      subject: 'Test',
+      text: 'Hello World, How are you!'
+    }
+
+    try {
+      await transporter.sendMail(mailOptions);
+      console.log('Email sent successfully.');
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
     
+
+
+
 
 
 
