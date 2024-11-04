0d60f16063b45d7b4bd6a02de744b8394332c54f
Hanees <ahmed.54.hanees31@gmail.com>
2024-11-04 20:43:56 +0000

    Changed to book in button in hero

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Images from './Images'
+import Link from 'next/link';
 
 const Hero = () => {
   return (
@@ -9,13 +10,12 @@ const Hero = () => {
         <p className="text-xl mb-6">
           Transform your vehicle into a sanctuary of style and technology with our premium aftermarket ambient lighting and CarPlay retrofitting services. At WAIS, we specialize in enhancing your driving experience by integrating cutting-edge technology and customizable lighting solutions that reflect your personal taste.
         </p>
-        <button className="mt-6 px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-purple-500 transition duration-300">
-          Browse Products
-        </button>
+        <Link href={'/contact'} className="mt-6 px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-purple-500 transition duration-300">
+          Get Booked In
+        </Link>
       </div>
 
       <div className="md:w-1/2 flex justify-center mt-8 md:mt-0 animate-float-up">
-
         <Images/>
       </div>
     </section>
