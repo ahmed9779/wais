3589a25de4766efd43a730213dde7465b9861fde
Hanees <ahmed.54.hanees31@gmail.com>
2024-11-04 21:07:26 +0000

    Changed api structure for vercel

--- app/ambients/page.tsx
+++ app/ambients/page.tsx
@@ -5,6 +5,7 @@ const page = () => {
   return (
     <div>
         <Navbar/>
+        <hr className="border-t-2 border-blue-500 max-w-[90%] md:max-w-[1280px] mx-auto animate-float-up"/>
 
 
     </div>
--- app/contact/ContactForm.tsx
+++ app/contact/ContactForm.tsx
@@ -23,7 +23,7 @@ const ContactForm = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('/api/contact', {
+      const response = await fetch('/pages/api', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
